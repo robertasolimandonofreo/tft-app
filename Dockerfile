b52a8048a1ed2b95f4624abed01177021f1980c1
Roberta <betasoliman@gmail.com>
2025-08-07 13:46:55 -0300

    - ajustar configuração para remover indicadores de desenvolvimento

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,5 @@ COPY --from=builder /app/.next ./.next
 COPY --from=builder /app/public ./public
 COPY --from=builder /app/package.json ./package.json
 COPY --from=builder /app/node_modules ./node_modules
-EXPOSE 3000
+EXPOSE 9000
 CMD ["npm", "start"]
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { useState } from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
+        <link rel="icon" type="image/png" href="/icon1.png" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+      </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
   )
