39701c4377548dcfa3ed391433b9726febdff601
Roberta <betasoliman@gmail.com>
2025-08-07 14:32:05 -0300

    feat(app): adicionar meta tags ao cabeçalho

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
+        <title>TFT Stats</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="TFT Stats" />
+        <meta charSet="UTF-8" />
+        <meta name="author" content="Roberta Soliman" />
+        <meta name="keywords" content="TFT, TFT Stats, TFT Stats App, League of Legends, TFT Stats App" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
         <link rel="icon" type="image/png" href="/icon1.png" />
